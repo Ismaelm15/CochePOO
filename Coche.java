88d1c39b9d40b1e8f6b67dafc0800dfa18acfb88
M15 <ismaelmelgar1999@gmail.com>
2020-12-21 14:30:40 +0100

    segundo commit de strangis

--- Coche.java
+++ Coche.java
@@ -10,5 +10,82 @@ package coche;
  * @author ismael
  */
 public class Coche {
+    private Motor motor;
+    private Puerta puertas;
+    private Rueda rueda1;
+    private Rueda rueda2;
+    private Rueda rueda3;
+    private Rueda rueda4;
+
+    public Coche(Motor motor, Puerta puertas, Rueda rueda1, Rueda rueda2, Rueda rueda3, Rueda rueda4) {
+        this.motor = motor;
+        this.puertas = puertas;
+        this.rueda1 = rueda1;
+        this.rueda2 = rueda2;
+        this.rueda3 = rueda3;
+        this.rueda4 = rueda4;
+    }
+
+    
+
+    public Coche() {
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public Puerta getPuertas() {
+        return puertas;
+    }
+
+    public Rueda getRueda1() {
+        return rueda1;
+    }
+
+    public Rueda getRueda2() {
+        return rueda2;
+    }
+
+    public Rueda getRueda3() {
+        return rueda3;
+    }
+
+    public Rueda getRueda4() {
+        return rueda4;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+
+    public void setPuertas(Puerta puertas) {
+        this.puertas = puertas;
+    }
+
+    public void setRueda1(Rueda rueda1) {
+        this.rueda1 = rueda1;
+    }
+
+    public void setRueda2(Rueda rueda2) {
+        this.rueda2 = rueda2;
+    }
+
+    public void setRueda3(Rueda rueda3) {
+        this.rueda3 = rueda3;
+    }
+
+    public void setRueda4(Rueda rueda4) {
+        this.rueda4 = rueda4;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche{" + "motor=" + motor + ", puertas=" + puertas + ", rueda1=" + rueda1 + ", rueda2=" + rueda2 + ", rueda3=" + rueda3 + ", rueda4=" + rueda4 + '}';
+    }
+
+
+
+
     
 }
--- Prueba.java
+++ Prueba.java
@@ -27,14 +27,32 @@ public class Prueba {
         
         Puerta p1 = new Puerta(false, v1);
         Puerta p2 = new Puerta(true, v2);
-        
-        System.out.println(p1);
-        
-        // Abrir la ventana de la puerta p1
-        // p1.getVentana() --> ventana
-        
-        p1.getVentana().abrir();
+//        
+//        System.out.println(p1);
+//        
+//        // Abrir la ventana de la puerta p1
+//        // p1.getVentana() --> ventana
+//        
+//        p1.getVentana().abrir();
+        Coche coche = new Coche();
+        coche.getMotor().apagar();
+        coche.getPuertas().cerrar();
+        coche.getRueda1().inflar();
+        coche.getRueda2().inflar();
+        coche.getRueda3().inflar();
+        coche.getRueda4().inflar();
 
+        Coche coche2 = new Coche(new Motor(false, 5), new Puerta(false,
+                new Ventana(false, false)), new Rueda(true, "verano"), 
+                new Rueda(true, "verano"),new Rueda(true, "verano"),
+                new Rueda(true, "verano"));
+        
+        coche2.getRueda3().setTipo("Invierno");
+               
+        Coche coche3 = new Coche(new Motor(false, 5), new Puerta(false,
+                new Ventana(false, false)), r1, 
+                new Rueda(true, "verano"),new Rueda(true, "verano"),
+                new Rueda(true, "verano"));
     }
 
 }
